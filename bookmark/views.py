2172200260ee369a9d6b65a8a232eaabff25dca3
mcPark <00like@hanmail.net>
2021-07-31 19:00:55 +0900

    'django002'

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+
+from .models import Bookmark
+
+class BookmarkListView(ListView):
+    model = Bookmark
+
+class BookmarkCreateView(CreateView):
+    model = Bookmark
